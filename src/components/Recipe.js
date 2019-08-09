599956aee6e1f07247abac5dbf939a72b41fa0f5
Rashad <rasad.mursudov@gmail.com>
2019-08-09 15:03:04 +0400

    Changed api-key

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import Header from './Header';
 import styles from '../styles/recipe.module.css'
 
-// const API_KEY = "d8ad7cd0ff1b93c3bc58a82ad7f98ea9"
+const API_KEY = "d8ad7cd0ff1b93c3bc58a82ad7f98ea9"
 // const API_KEY = "a452c267a5f31de9268dab05c0fbe965"
 // const API_KEY = "58b9e45ff6922f7ed5367bbe466ec82d"
 // const API_KEY = "0abf5b4ffed1e40d2b7039266dac7086"
-const API_KEY = "a43b5e59dc13c4677cb3960d471e50e5"
+// const API_KEY = "a43b5e59dc13c4677cb3960d471e50e5"
 
 
 
