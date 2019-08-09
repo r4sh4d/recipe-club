8a9b0ec519ffdef1fd9225173e99ff3b60591fa8
Rashad <rasad.mursudov@gmail.com>
2019-08-09 15:52:59 +0400

    Added logo to the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react'
 import styles from '../styles/header.module.css'
 
 const Header = () => (
-  <header className={styles.header}>Recipe Collection
-    <span className={styles.author}>by RASHAD MURSHUDOV</span>
+  <header className={styles.header}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recipe Collection</h1>
+      <span className={styles.author}>by RASHAD MURSHUDOV</span>
+    </div>
+    <div className={styles.logoContainer}>
+      <img className={styles.logo} src={require('../images/logo.png')} />
+    </div>
   </header>
 )
 
