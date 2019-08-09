4de9befbbe9c875a49a12f578e367c717ae7ed82
Rashad <rasad.mursudov@gmail.com>
2019-08-09 14:42:41 +0400

    Changed recipe hover effects

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,17 +4,15 @@ import { Link } from 'react-router-dom'
 
 const RecipeCard = props => {
   return (
-    <div className={styles.outerContainer}>
-      <div className={styles.recipeContainer}>
-        <img src={props.imgUrl} className={styles.recipeImage} alt={props.title} />
-        <h3 className={styles.recipeTitle}>{props.title.length < 22 ? `${props.title}` : `${props.title.substring(0, 22)}...`}</h3>
-        <p className={styles.recipePublisher}>Publisher: {props.publisher}</p>
-        <Link to={{
-          pathname: `/recipe/${props.id}`,
-          state: { recipe: props.title }
+    <div className={styles.recipeContainer}>
+      <img src={props.imgUrl} className={styles.recipeImage} alt={props.title} />
+      <h3 className={styles.recipeTitle}>{props.title.length < 22 ? `${props.title}` : `${props.title.substring(0, 22)}...`}</h3>
+      <p className={styles.recipePublisher}>Publisher: {props.publisher}</p>
+      <Link to={{
+        pathname: `/recipe/${props.id}`,
+        state: { recipe: props.title }
 
-        }} className={styles.more}>View Recipe</Link>
-      </div>
+      }} className={styles.more}>View More</Link>
     </div>
   )
 }
