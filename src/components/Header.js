5305de117291f2bbebef82bca903a14238cc9c14
Rashad <rasad.mursudov@gmail.com>
2019-08-10 20:47:00 +0400

    Made header's position fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,18 @@ import styles from '../styles/header.module.css'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.container}>
-      <h1 className={styles.title}>Recipe Collection</h1>
-      <span className={styles.author}>by RASHAD MURSHUDOV</span>
-    </div>
-    <div className={styles.logoContainer}>
-      <Link className={styles.logoLink} to="/"><img className={styles.logo} src={require('../images/logo.png')} alt="Logo" /></Link>
+  <header className={styles.outerContainer}>
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Recipe Collection</h1>
+        <span className={styles.author}>by RASHAD MURSHUDOV</span>
+      </div>
+      <div className={styles.logoContainer}>
+        <Link className={styles.logoLink} to="/"><img className={styles.logo} src={require('../images/logo.png')} alt="Logo" /></Link>
+      </div>
     </div>
   </header>
+
 )
 
 export default Header
