1029f07fbb9ac261d0bcfab871e09774b97c2cef
Rashad <rasad.mursudov@gmail.com>
2019-09-02 16:08:46 +0400

    Added header scroll bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/header.module.css'
 import { Link } from 'react-router-dom'
 
-const Header = () => (
-  <header className={styles.outerContainer}>
-    <div className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Recipe Collection</h1>
-        <span className={styles.author}>by RASHAD MURSHUDOV</span>
-      </div>
-      <div className={styles.logoContainer}>
-        <Link className={styles.logoLink} to="/"><img className={styles.logo} src={require('../images/logo.png')} alt="Logo" /></Link>
+
+const Header = () => {
+
+  // const [scrollWidth, setScrollWith] = useState('10')
+
+
+  // setTimeout(() => console.log(document.body.scrollHeight), 3000)
+
+
+
+  window.onscroll = function (e) {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll').style.width = scrolled + '%'
+  }
+  // setInterval(() => {
+
+  //   console.log(document.body.offsetHeight)
+  // }, 200)
+
+  return (
+    <div>
+      <header className={styles.outerContainer}>
+        <div className={styles.header}>
+          <div className={styles.container}>
+            <h1 className={styles.title}>Recipe Collection</h1>
+            <span className={styles.author}>by RASHAD MURSHUDOV</span>
+          </div>
+          <div className={styles.logoContainer}>
+            <Link className={styles.logoLink} to="/"><img className={styles.logo} src={require('../images/logo.png')} alt="Logo" /></Link>
+          </div>
+        </div>
+      </header>
+      <div className={styles.scrollContainer}>
+        <div id="scroll" className={styles.scroll}></div>
       </div>
     </div>
-  </header>
-
-)
+  )
+}
 
 export default Header
